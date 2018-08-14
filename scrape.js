e5e8d04d1f05ff08ec5e5cd99bc620dc5091fe5d
Luka Bulatovic <luka.bulatovic.w@gmail.com>
2018-08-14 10:13:57 +0200

    Added emulated Iphone

--- scrape.js
+++ scrape.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
 const puppeteer = require('puppeteer');
+
+const devices = require('puppeteer/DeviceDecriptors');
+const iPhone = devices['iPhone 6'];
+
 let port = process.env.PORT || 3000;
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
